436c4626736bb1a50abd0912f781ab8dd05a283c
curiousitymars <84633190+curiousitymars@users.noreply.github.com>
2022-05-27 18:43:31 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: 'jpg',
+    jpg_quality: 90
+});
+camera = document.getElementById("camera");
+Webcam.attach('#camera');
+console.log('ml5 version:', ml5.version);
+function take_snapshot() {
+    Webcam.snap(function(data_uri)
+    {
+
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'">';
+
+    });
+} 
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/I0t7FJYBJ/model.json', modelLoaded);
